26a7816b45e1ebfb9aa8d9108fe1d33fdec367a7
Josh Zheng <denial@gmail.com>
2017-02-10 17:26:52 -0800

    code to classify image

---
+++ python/classify_image.py
@@ -0,0 +1,23 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv, find_dotenv
+from watson_developer_cloud import VisualRecognitionV3
+from pprint import pprint
+
+def main():
+  load_dotenv(find_dotenv())
+  api_key=os.environ.get("API_KEY")
+
+  sdk_vr = VisualRecognitionV3('2016-05-20', api_key=api_key)
+
+  with open(join(dirname(__file__), 
+                 '../data/testing/elmo.jpg'), 'rb') as elmo:
+
+    response = sdk_vr.classify(elmo,
+                        classifier_ids=['sesame_street_1968640558'],
+                        threshold=0.1)
+
+    pprint(response)
+
+if __name__ == "__main__":
+    main()
