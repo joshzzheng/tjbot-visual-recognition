f2a499b2375c438a30c85d55fddf7682d7762fa5
Josh Zheng <denial@gmail.com>
2017-02-18 17:10:45 -0800

    recognizing character

--- tjbot/run.js
+++ tjbot/run.js
@@ -1,6 +1,14 @@
-let RaspiCam = require('raspicam');
+const RaspiCam = require('raspicam');
+const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+const fs = require('fs');
+const config = require('../config.js')
 
-let camera = new RaspiCam({
+const vr = new VisualRecognitionV3({
+  api_key: config.vrApiKey,
+  version_date: '2016-05-19'
+});
+
+const camera = new RaspiCam({
   mode: "photo",
   output: "./photo/image.jpg",
   encoding: "jpg",
@@ -8,13 +16,27 @@ let camera = new RaspiCam({
 });
 
 const formatTimestamp = (timestamp) => {
-  let date = new Date(timestamp*1000);
+  let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = "0" + date.getMinutes();
   let seconds = "0" + date.getSeconds();
   return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 }
 
+const recognizeCharacter = (imagePath) => {
+  const params = {
+    images_file: fs.createReadStream(imagePath),
+    classifier_ids: [config.classifierId],
+    threshold: 0
+  }; 
+
+  vr.classify(params, function(err, res) {
+    if (err)
+      console.log(err);
+    else
+      console.log(JSON.stringify(res, null, 2));
+  });
+}
 
 camera.on("start", function( err, timestamp ){
   console.log("photo started at " + formatTimestamp(timestamp) );
